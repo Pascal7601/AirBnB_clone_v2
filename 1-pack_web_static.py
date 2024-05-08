14d9b80ffce56d8bdbcd7025048490cf3e471ce5
Pascal7601 <levispascal63@gmail.com>
2024-05-08 09:00:24 -0700

    creating an archive for web static folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-#scrpt that creates an achive file 
+# scrpt that creates an achive file
 
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """ creates an archive file for web static
-    folder 
+    folder
     """
     try:
         local("mkdir versions")
 
         time = datetime.now()
         time_str = time.strftime("%Y%m%d%H%M%S")
+        file_name = 'versions/web_static_'
 
-        local("tar -czvf versions/web_static_.{}.tgz web_static".format(time_str)) 
+        local("tar -czvf {}.{}.tgz web_static".format(filename, time_str))
 
     except Exception:
         return None
