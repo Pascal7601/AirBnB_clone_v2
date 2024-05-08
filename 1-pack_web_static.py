ed6b9e5b331c9ae805608588c93e19ab4510673a
Pascal7601 <levispascal63@gmail.com>
2024-05-08 08:48:56 -0700

    local

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#scrpt that creates an achive file 
+
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """ creates an archive file for web static
+    folder 
+    """
+    try:
+        local("mkdir versions")
+
+        time = datetime.now()
+        time_str = time.strftime("%Y%m%d%H%M%S")
+
+        local("tar -czvf versions/web_static_.{}.tgz web_static".format(time_str)) 
+
+    except Exception:
+        return None
---
+++ fabfile.py
@@ -0,0 +1,6 @@
+from fabric.api import run, local, put, get
+
+def deploy():
+    run("whoami")
+
+#def my_host():
