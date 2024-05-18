753184303143fa812a6e079f38782a39ab07d2c0
Pascal7601 <levispascal63@gmail.com>
2024-05-18 03:54:40 -0700

    intro

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,19 +5,18 @@ script that starts a flask web app
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-"""
-a function to start a web flask
-"""
 
+@app.route('/', strict_slashes=False)
 def index():
     """
     function that starts a web flask
     application
     """
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
