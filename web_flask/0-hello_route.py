6f4b935c541b8551f1a2935cb670e2bf7775d060
Pascal7601 <levispascal63@gmail.com>
2024-05-18 03:48:40 -0700

    intro to flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,6 +8,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
+"""
+a function to start a web flask
+"""
+
 def index():
     """
     function that starts a web flask
