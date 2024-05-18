810cabda76e7b8e986d68ae5b513b10e5457434a
Pascal7601 <levispascal63@gmail.com>
2024-05-18 12:02:41 -0700

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+script that takes in a variable
+on the route
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    displays content at the root folder
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    displays content in the hbnb route
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    navigates to the c section of the
+    browser
+    """
+    new_text = ' '.join(text.split('_'))
+    return f"C {escape(new_text)}"
+
+
+@app.route('/python/<text>')
+def py_route(text):
+    """
+    displays content in the python
+    section keyed by the user
+    """
+    if text is None:
+       new_text = 'is cool'
+    else:
+        new_text = ' '.join(text.split('_'))
+    return f"Python {new_text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
