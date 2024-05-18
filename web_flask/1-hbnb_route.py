274b155d2c9de7f8a75c1725739001b4e210129c
Pascal7601 <levispascal63@gmail.com>
2024-05-18 08:15:43 -0700

    start flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,6 +8,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
     """
@@ -24,6 +25,5 @@ def hbnb_page():
     return "HBNB"
 
 
-if  __name__ == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-    
