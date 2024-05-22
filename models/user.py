9145a8a2bfc99d02ca2ee9c25f4a2c196b10abc4
Pascal7601 <levispascal63@gmail.com>
2024-05-22 19:22:30 +0300

    user city relationship

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ This module defines a class User
 """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """
@@ -15,3 +16,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+
+    places = relationship('Place',backref='user', cascade='all delete-orphan')
