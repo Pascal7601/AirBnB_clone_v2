6904095dca0d2f6322adead32ed930043478c3bd
Pascal7601 <levispascal63@gmail.com>
2024-05-23 00:40:03 +0300

    theirs

--- models/base_model.py
+++ models/base_model.py
@@ -29,11 +29,11 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
-            if 'updated_at' not in kwargs:
+            if 'updated_at' in kwargs:
                 kwargs['updated_at'] = datetime.strptime(
                                                         kwargs['updated_at'],
                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            if 'created_at' not in kwargs:
+            if 'created_at' in kwargs:
                 kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                         '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
