07d8976f094069d764a48bbd2979b6e31f800caa
Pascal7601 <levispascal63@gmail.com>
2024-05-22 23:14:21 +0300

    changes

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+State Module for HBNB project
+"""
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """
     amenity class
     """
