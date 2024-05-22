9f845998a447be89a8dfbc1fd7549ef87e0ddcba
Pascal7601 <levispascal63@gmail.com>
2024-05-22 21:45:23 +0300

    amenity not defined

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
-
+from sqlalchemy import Column, String
 
 class Amenity(BaseModel):
     """
